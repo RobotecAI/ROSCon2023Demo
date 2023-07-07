60cd018522a0368a20cdd4afb72bd1cedf349d41
Michał Pełka <michal.pelka@robotec.ai>
2023-07-07 17:19:21 +0200

    Initial scene setup
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>

---
+++ Gem/ROSCon2023Demo_files.cmake
@@ -0,0 +1,9 @@
+# This file is copied during engine registration. Edits to this file will be lost next
+# time a registration happens.
+
+set(FILES
+    Include/ROSCon2023Demo/ROSCon2023DemoBus.h
+    Source/ROSCon2023DemoSystemComponent.cpp
+    Source/ROSCon2023DemoSystemComponent.h
+    enabled_gems.cmake
+)

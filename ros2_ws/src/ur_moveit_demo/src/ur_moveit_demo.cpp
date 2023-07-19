8e144c6465d609ecc8a6946fd9eb1430d68d6ab8
Kacper Lewandowski <kacper.lewandowski@robotec.ai>
2023-07-19 16:14:40 +0200

    cleanup
    Signed-off-by: Kacper Lewandowski <kacper.lewandowski@robotec.ai>

--- ros2_ws/src/ur_moveit_demo/src/ur_moveit_demo.cpp
+++ ros2_ws/src/ur_moveit_demo/src/ur_moveit_demo.cpp
@@ -181,35 +181,10 @@ int main(int argc, char * argv[])
 
 
   auto goal = control_msgs::action::GripperCommand::Goal();
-
   goal.command.position = 0.0;
   goal.command.max_effort = 10000.0;
-
   auto future = client_ptr->async_send_goal(goal);
-
   future.wait();
-
-  //  while (!client->wait_for_service(1s)) {
-  //   if (!rclcpp::ok()) {
-  //     RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
-  //     return 0;
-  //   }
-  //   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
-  // }
-
-
-  // auto result = client->async_send_request(request);
-  // // Wait for the result.
-  // if (rclcpp::spin_until_future_complete(node, result) ==
-  //   rclcpp::FutureReturnCode::SUCCESS)
-  // {
-  //   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Gripping succesfull");
-  // } else {
-  //   RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_two_ints");
-  // }
-
-
-
 
 
   auto const target_pose_2 = [] {

521bdafd2dc794b694e75c44a6d3c436bf37fd9a
Michał <michal.pelka@robotec.ai>
2023-08-18 13:45:14 +0200

    Simple orchestartion

---
+++ ros2_ws/src/o3de_fleet_nav/launch/waypoint.launch.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2018 Intel Corporation
+# Copyright (c) Contributors to the Open 3D Engine Project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    ways = Node(
+            package='o3de_fleet_nav',
+            executable='follow_waypoints',
+            output='log'
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+
+    ld.add_action(ways)
+
+    return ld

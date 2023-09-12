71310cb7cbee661cd127d1a79923c699c862e2a7
Michał <michal.pelka@robotec.ai>
2023-09-12 17:01:00 +0200

    Adjusted scene to UR20, change ROS2 UR Driver to Robotec's fork
    Signed-off-by: Michał <michal.pelka@robotec.ai>

--- ros2_ws/src/UR20_moveit_config/launch/moveit_rviz.launch.py
+++
@@ -1,9 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_moveit_rviz_launch
-
-
-def generate_launch_description():
-        
-    moveit_config = MoveItConfigsBuilder("ur20", package_name="UR20_moveit_config").to_moveit_configs()
-    print (moveit_config)
-    return generate_moveit_rviz_launch(moveit_config)

b34c1bb3bacb644ef0b53fecc9efbe1dc2f3a1a7
Adam Dąbrowski <adam.dabrowski@robotec.ai>
2023-09-20 18:42:09 +0200

    Otto deliberation  (#88)
    * otto deliberation compiling
    * added reverse control to task
    * deliberation - get lane and set tasks
    * otto autonomy work
    * removed comments
    * removed unused old msg
    
    ---------
    
    Signed-off-by: Adam Dabrowski <adam.dabrowski@robotec.ai>

---
+++ ros2_ws/src/otto_deliberation/include/otto_deliberation/robot_status.h
@@ -0,0 +1,36 @@
+
+#include <rclcpp/rclcpp.hpp>
+
+#pragma once
+
+enum class RobotCargoStatus
+{
+	CARGO_EMPTY,
+	CARGO_LOADED
+};
+
+enum class RobotTaskStatus
+{
+	IDLE,
+	GO_TO_PICK_UP,
+	APPROACH_PICKUP,
+	WAIT_LOAD,
+	EVACUATE_FROM_PICK_UP,
+	GO_TO_WRAPPING,
+	DO_WRAPPING,
+	GO_TO_UNLOAD,
+	APPROACH_UNLOAD,
+	WAIT_UNLOAD,
+	EVACUATE_FROM_UNLOAD
+};
+
+struct RobotStatus
+{
+	RobotTaskStatus m_taskStatus;
+	RobotCargoStatus m_cargoStatus;
+	std::string m_currentTaskKey;
+	RobotStatus() {
+		m_cargoStatus = RobotCargoStatus::CARGO_EMPTY;
+		m_taskStatus = RobotTaskStatus::IDLE;
+	}
+};

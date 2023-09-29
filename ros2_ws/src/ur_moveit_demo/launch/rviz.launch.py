144df2f52c7e6173cfd1d055ee8f8e74adc96fc9
Michał Pełka <michal.pelka@robotec.ai>
2023-09-29 11:35:06 +0200

    Moveit charming (#154)
    * snake charming for moveit for better behavior
    
    Signed-off-by: Adam Dabrowski <adam.dabrowski@robotec.ai>
    
    * reactivate robot detection
    
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>
    
    ---------
    
    Signed-off-by: Adam Dabrowski <adam.dabrowski@robotec.ai>
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>
    Co-authored-by: Adam Dabrowski <adam.dabrowski@robotec.ai>

--- ros2_ws/src/ur_moveit_demo/launch/rviz.launch.py
+++ ros2_ws/src/ur_moveit_demo/launch/rviz.launch.py
@@ -190,7 +190,7 @@ def launch_setup(context, *args, **kwargs):
         "moveit_manage_controllers": False,
         "trajectory_execution.allowed_execution_duration_scaling": 1.2,
         "trajectory_execution.allowed_goal_duration_margin": 0.5,
-        "trajectory_execution.allowed_start_tolerance": 0.01,
+        "trajectory_execution.allowed_start_tolerance": 0.1,
     }
 
     planning_scene_monitor_parameters = {
--- ros2_ws/src/ur_moveit_demo/src/mtc.cpp
+++ ros2_ws/src/ur_moveit_demo/src/mtc.cpp
@@ -49,180 +49,200 @@ constexpr char PalletNamePrefix[] = "EuroPallet";
 constexpr char PickedBoxName[] = "PickedBox";
 
 const std::vector<Eigen::Vector3f> Pattern{
-//    { -1.f, 0.5, 1.f }, { 0.f, 0.5f, 1.f }, { 1.f, 0.5f, 1.f },{ -1.f, -0.5f, 1.f }, { 0.f, -0.5f, 1.f }, { 1.f, -0.5f, 1.f },
-//    { -1.f, 0.5, 2.f }, { 0.f, 0.5f, 2.f }, { 1.f, 0.5f, 2.f }, { -1.f, -0.5f, 2.f }, { 0.f, -0.5f, 2.f }, { 1.f, -0.5f, 2.f },
-        { -1.0f, 0.5f, 1.f },  { 0.0f, 0.5f, 1.f }, { 1.0f, 0.5f, 1.f }, { -1.0f, -0.5f, 1.f }, { 0.0f, -0.5f, 1.f }, { 1.0f, -0.5f, 1.f },
-        { -1.0f, 0.5f, 2.f },  { 0.0f, 0.5f, 2.f }, { 1.0f, 0.5f, 2.f }, { -1.0f, -0.5f, 2.f }, { 0.0f, -0.5f, 2.f }, { 1.0f, -0.5f, 2.f },
-        { -1.0f, 0.5f, 3.f },  { 0.0f, 0.5f, 3.f }, { 1.0f, 0.5f, 3.f }, { -1.0f, -0.5f, 3.f }, { 0.0f, -0.5f, 3.f }, { 1.0f, -0.5f, 3.f },
+	//    { -1.f, 0.5, 1.f }, { 0.f, 0.5f, 1.f }, { 1.f, 0.5f, 1.f },{ -1.f, -0.5f, 1.f }, { 0.f, -0.5f, 1.f }, { 1.f,
+	//    -0.5f, 1.f },
+	//    { -1.f, 0.5, 2.f }, { 0.f, 0.5f, 2.f }, { 1.f, 0.5f, 2.f }, { -1.f, -0.5f, 2.f }, { 0.f, -0.5f, 2.f }, { 1.f,
+	//    -0.5f, 2.f },
+	{ -1.0f, 0.5f, 1.f },  { 0.0f, 0.5f, 1.f },  { 1.0f, 0.5f, 1.f },  { -1.0f, -0.5f, 1.f }, { 0.0f, -0.5f, 1.f },
+	{ 1.0f, -0.5f, 1.f },  { -1.0f, 0.5f, 2.f }, { 0.0f, 0.5f, 2.f },  { 1.0f, 0.5f, 2.f },   { -1.0f, -0.5f, 2.f },
+	{ 0.0f, -0.5f, 2.f },  { 1.0f, -0.5f, 2.f }, { -1.0f, 0.5f, 3.f }, { 0.0f, 0.5f, 3.f },   { 1.0f, 0.5f, 3.f },
+	{ -1.0f, -0.5f, 3.f }, { 0.0f, -0.5f, 3.f }, { 1.0f, -0.5f, 3.f },
 
 };
+
+using namespace std::chrono_literals;
 
 class RobotDetection
 {
 public:
-    RobotDetection(rclcpp::Node::SharedPtr node, std::string ns)
-        : m_node(node)
-        , m_ns(ns)
-    {
-        m_visionSystem = std::make_shared<Camera::GroundTruthCamera>(
-            node, "/" + ns + "/camera_pickup/detections3D", "/" + ns + "/camera_drop/detections3D", ns);
+	RobotDetection(rclcpp::Node::SharedPtr node, std::string ns) : m_node(node), m_ns(ns) {
+		m_visionSystem = std::make_shared<Camera::GroundTruthCamera>(node, "/" + ns + "/camera_pickup/detections3D",
+		                                                             "/" + ns + "/camera_drop/detections3D", ns);
 
-        m_timer = m_node->create_wall_timer(std::chrono::milliseconds(50), std::bind(&RobotDetection::TimerCallback, this));
+		m_timer =
+		    m_node->create_wall_timer(std::chrono::milliseconds(50), std::bind(&RobotDetection::TimerCallback, this));
 
-        m_waitTime = std::chrono::seconds(m_node->get_parameter("wait_time").as_int());
-        m_numOfBoxes = m_node->get_parameter("num_of_boxes").as_int();
-        m_tolerance = m_node->get_parameter("pose_tolerance").as_double();
-    }
+		m_waitTime = std::chrono::seconds(m_node->get_parameter("wait_time").as_int());
+		m_numOfBoxes = m_node->get_parameter("num_of_boxes").as_int();
+		m_tolerance = m_node->get_parameter("pose_tolerance").as_double();
+	}
 
 private:
-    void TimerCallback()
-    {
-        if (m_visionSystem->IsRobotPresent())
-        {
-            if (!m_isExecuting && m_robotLeft)
-            {
-                if (m_robotName == "")
-                {
-                    RCLCPP_INFO(m_node->get_logger(), "Robot detected");
-                    m_robotName = m_visionSystem->GetRobotName();
-                }
-                geometry_msgs::msg::Pose robotPose = *m_visionSystem->getObjectPose(m_robotName);
-                Eigen::Vector3d currentRobotPose{ robotPose.position.x, robotPose.position.y, robotPose.position.z };
-                Eigen::Vector3d difference = currentRobotPose - m_lastRobotPose;
-                if (difference.norm() > m_tolerance)
-                {
-                    lastUpdate = std::chrono::system_clock::now();
-                }
-                m_lastRobotPose = currentRobotPose;
-                if (std::chrono::system_clock::now() - lastUpdate > m_waitTime)
-                {
-                    RCLCPP_INFO(m_node->get_logger(), "Starting palletization");
-                    m_robotLeft = false;
-                    m_isExecuting = true;
-                    std::thread runner(
-                        [&]()
-                        {
-                            execute();
-                        });
-                    runner.detach();
-                }
-            }
-        }
-        else
-        {
-            m_robotName = "";
-            m_robotLeft = true;
-        }
-    }
+	void TimerCallback() {
+		if (m_visionSystem->IsRobotPresent()) {
+			if (!m_isExecuting && m_robotLeft) {
+				if (m_robotName == "") {
+					RCLCPP_INFO(m_node->get_logger(), "Robot detected");
+					m_robotName = m_visionSystem->GetRobotName();
+				}
+				geometry_msgs::msg::Pose robotPose = *m_visionSystem->getObjectPose(m_robotName);
+				Eigen::Vector3d currentRobotPose{ robotPose.position.x, robotPose.position.y, robotPose.position.z };
+				Eigen::Vector3d difference = currentRobotPose - m_lastRobotPose;
+				if (difference.norm() > m_tolerance) {
+					lastUpdate = std::chrono::system_clock::now();
+				}
+				m_lastRobotPose = currentRobotPose;
+				if (std::chrono::system_clock::now() - lastUpdate > m_waitTime) {
+					RCLCPP_INFO(m_node->get_logger(), "Starting palletization");
+					m_robotLeft = false;
+					m_isExecuting = true;
+					std::thread runner([&]() { execute(); });
+					runner.detach();
+				}
+			}
+		} else {
+			m_robotName = "";
+			m_robotLeft = true;
+		}
+	}
 
-    void execute()
-    {
-        moveit::planning_interface::PlanningSceneInterface planning_scene_interface("/" + m_ns);
-        planning_scene_interface.removeCollisionObjects(planning_scene_interface.getKnownObjectNames());
+	std::vector<Eigen::Vector3f> putBoxesInPlaces(std::shared_ptr<TaskConstructor::MTCController> mtc_task_node,
+	                                              std::shared_ptr<Gripper::GripperController> gripperController,
+	                                              const std::vector<Eigen::Vector3f>& targets) {
+		bool previousFailed = false;
+		std::vector<Eigen::Vector3f> failedBoxes;
+		for (size_t i = 0; i < targets.size(); i++) {
+			if (previousFailed) {
+				mtc::Task taskPark = mtc_task_node->createTaskPark(m_ns);
+				mtc_task_node->doTask(taskPark);
+				previousFailed = false;
+			}
 
-        auto mtc_task_node = std::make_shared<TaskConstructor::MTCController>(m_node, m_ns);
-        auto gripperController = std::make_shared<Gripper::GripperController>(m_node, "/" + m_ns + "/gripper_server");
+			auto const& address = targets[i];
+			std::optional<geometry_msgs::msg::Pose> myClosestBox;
+			std::vector<geometry_msgs::msg::Pose> allBoxesOnPallet;
 
-        auto slashLocation = m_robotName.find("/");
-        auto cargoPublisher =
-            m_node->create_publisher<std_msgs::msg::Bool>("/" + m_robotName.substr(0, slashLocation + 1) + "cargo_status", 1);
+			myClosestBox = m_visionSystem->getClosestBox();
+			allBoxesOnPallet = m_visionSystem->getAllBoxesOnPallet();
 
-        for (int i = 0; i < m_numOfBoxes; i++)
-        {
-            auto const& address = Pattern[i] * 1.2f;
-            std::optional<geometry_msgs::msg::Pose> myClosestBox;
-            std::vector<geometry_msgs::msg::Pose> allBoxesOnPallet;
+			auto palletPose = m_visionSystem->getObjectPose("/EuroPallet");
+			if (!palletPose) {
+				break;
+			}
 
-            myClosestBox = m_visionSystem->getClosestBox();
-            allBoxesOnPallet = m_visionSystem->getAllBoxesOnPallet();
+			if (!myClosestBox) {
+				RCLCPP_ERROR(m_node->get_logger(), "No box found");
+				failedBoxes.push_back(address);
+				previousFailed = true;
+				continue;
+			}
 
-            auto palletPose = m_visionSystem->getObjectPose("/EuroPallet");
-            if (!palletPose)
-            {
-                break;
-            }
+			auto stage_state_current = std::make_unique<mtc::stages::CurrentState>("current");
 
-            if (!myClosestBox)
-            {
-                RCLCPP_ERROR(m_node->get_logger(), "No box found");
-                break;
-            }
+			mtc::Task taskGrab = mtc_task_node->createTaskGrab(*myClosestBox, PickedBoxName, m_ns);
+			if (!mtc_task_node->doTask(taskGrab)) {
+				gripperController->Release();
+				failedBoxes.push_back(address);
+				previousFailed = true;
+				continue;
+			}
+			gripperController->Grip();
+			std::this_thread::sleep_for(50ms);
+			auto taskDrop =
+			    mtc_task_node->createTaskDrop(address, PickedBoxName, *palletPose, *myClosestBox, allBoxesOnPallet, m_ns);
+			if (!mtc_task_node->doTask(taskDrop)) {
+				gripperController->Release();
+				failedBoxes.push_back(address);
+				previousFailed = true;
+				continue;
+			}
+			gripperController->Release();
+			std::this_thread::sleep_for(50ms);
+		}
+		return failedBoxes;
+	}
 
-            auto stage_state_current = std::make_unique<mtc::stages::CurrentState>("current");
+	void execute() {
+		moveit::planning_interface::PlanningSceneInterface planning_scene_interface("/" + m_ns);
+		planning_scene_interface.removeCollisionObjects(planning_scene_interface.getKnownObjectNames());
 
-            mtc::Task taskGrab = mtc_task_node->createTaskGrab(*myClosestBox, PickedBoxName, m_ns);
-            if (!mtc_task_node->doTask(taskGrab))
-            {
-                break;
-            }
-            gripperController->Grip();
-            auto taskDrop = mtc_task_node->createTaskDrop(address, PickedBoxName, *palletPose, *myClosestBox, allBoxesOnPallet, m_ns);
-            if (!mtc_task_node->doTask(taskDrop))
-            {
-                gripperController->Release();
-                break;
-            }
-            gripperController->Release();
-        }
+		auto mtc_task_node = std::make_shared<TaskConstructor::MTCController>(m_node, m_ns);
+		auto gripperController = std::make_shared<Gripper::GripperController>(m_node, "/" + m_ns + "/gripper_server");
 
-        mtc::Task taskPark = mtc_task_node->createTaskPark(m_ns);
-        mtc_task_node->doTask(taskPark);
+		if (m_numOfBoxes > Pattern.size()) {
+			RCLCPP_WARN_STREAM(m_node->get_logger(), "Reducing box task to available pattern size");
+			m_numOfBoxes = Pattern.size();
+		}
 
-        std_msgs::msg::Bool cargoFullMessage;
-        cargoFullMessage.data = true;
-        cargoPublisher->publish(cargoFullMessage);
+		std::vector<Eigen::Vector3f> boxesTargetPlaces;
+		boxesTargetPlaces.resize(m_numOfBoxes);
+		float scaling = 1.2f;
+		std::transform(Pattern.begin(), Pattern.begin() + m_numOfBoxes, boxesTargetPlaces.begin(),
+		               [scaling](Eigen::Vector3f element) { return element * scaling; });
+		auto failedBoxes = putBoxesInPlaces(mtc_task_node, gripperController, boxesTargetPlaces);
 
-        m_isExecuting = false;
-    }
+		// Retry once for failed boxes
+		failedBoxes = putBoxesInPlaces(mtc_task_node, gripperController, failedBoxes);
+		if (failedBoxes.size() > 0) {
+			RCLCPP_WARN_STREAM(m_node->get_logger(), "Failed boxes " << failedBoxes.size());
+		}
 
-    rclcpp::Node::SharedPtr m_node;
-    std::shared_ptr<Camera::GroundTruthCamera> m_visionSystem;
-    rclcpp::TimerBase::SharedPtr m_timer;
-    std::string m_ns;
+		mtc::Task taskPark = mtc_task_node->createTaskPark(m_ns);
+		mtc_task_node->doTask(taskPark);
 
-    Eigen::Vector3d m_lastRobotPose{ 0, 0, 0 };
-    std::string m_robotName;
-    bool m_isExecuting = false;
-    bool m_robotLeft = true;
+		auto slashLocation = m_robotName.find("/");
+		auto cargoPublisher = m_node->create_publisher<std_msgs::msg::Bool>(
+		    "/" + m_robotName.substr(0, slashLocation + 1) + "cargo_status", 1);
+		std_msgs::msg::Bool cargoFullMessage;
+		cargoFullMessage.data = true;
+		cargoPublisher->publish(cargoFullMessage);
 
-    std::chrono::time_point<std::chrono::system_clock> lastUpdate{ std::chrono::system_clock::now() };
+		m_isExecuting = false;
+	}
 
-    double m_tolerance = 0.01;
-    std::chrono::seconds m_waitTime{ 3 };
-    int m_numOfBoxes = 1;
+	rclcpp::Node::SharedPtr m_node;
+	std::shared_ptr<Camera::GroundTruthCamera> m_visionSystem;
+	rclcpp::TimerBase::SharedPtr m_timer;
+	std::string m_ns;
+
+	Eigen::Vector3d m_lastRobotPose{ 0, 0, 0 };
+	std::string m_robotName;
+	bool m_isExecuting = false;
+	bool m_robotLeft = true;
+
+	std::chrono::time_point<std::chrono::system_clock> lastUpdate{ std::chrono::system_clock::now() };
+
+	double m_tolerance = 0.01;
+	std::chrono::seconds m_waitTime{ 3 };
+	int m_numOfBoxes = 1;
 };
 
-int main(int argc, char** argv)
-{
-    std::mutex locationsMutex;
-    std::optional<geometry_msgs::msg::Pose> closestBox;
-    std::map<std::string, geometry_msgs::msg::Pose> objectPoses;
-    std::vector<geometry_msgs::msg::Pose> allBoxesOnPallet;
+int main(int argc, char** argv) {
+	std::mutex locationsMutex;
+	std::optional<geometry_msgs::msg::Pose> closestBox;
+	std::map<std::string, geometry_msgs::msg::Pose> objectPoses;
+	std::vector<geometry_msgs::msg::Pose> allBoxesOnPallet;
 
-    rclcpp::init(argc, argv);
+	rclcpp::init(argc, argv);
 
-    rclcpp::NodeOptions options;
-    options.automatically_declare_parameters_from_overrides(true);
+	rclcpp::NodeOptions options;
+	options.automatically_declare_parameters_from_overrides(true);
 
-    rclcpp::executors::MultiThreadedExecutor executor_mtc;
+	rclcpp::executors::MultiThreadedExecutor executor_mtc;
 
-    auto const node =
-        std::make_shared<rclcpp::Node>("mtc_moveit", rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true));
+	auto const node = std::make_shared<rclcpp::Node>(
+	    "mtc_moveit", rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true));
 
-    rclcpp::executors::SingleThreadedExecutor executor;
-    executor.add_node(node);
-    auto spinner = std::thread(
-        [&executor]()
-        {
-            executor.spin();
-        });
+	rclcpp::executors::SingleThreadedExecutor executor;
+	executor.add_node(node);
+	auto spinner = std::thread([&executor]() { executor.spin(); });
 
-    auto parameter = node->get_parameter("ns");
-    auto ns = parameter.as_string();
+	auto parameter = node->get_parameter("ns");
+	auto ns = parameter.as_string();
 
-    RobotDetection robotDetection(node, ns);
+	RobotDetection robotDetection(node, ns);
 
-    spinner.join();
-    rclcpp::shutdown();
-    return 0;
+	spinner.join();
+	rclcpp::shutdown();
+	return 0;
 }

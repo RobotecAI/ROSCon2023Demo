b892a4c259f19fe6366131462f4b85c7ec8dac7b
Artur Kamieniecki <130671280+arturkamieniecki@users.noreply.github.com>
2023-10-09 09:28:55 +0200

    Support for otto1500 (#168)
    * Support for otto1500 without nav2
    * Add support for nav2
    * Test nav2 path
    * Add param files
    ---------
    
    Signed-off-by: Artur Kamieniecki <130671280+arturkamieniecki@users.noreply.github.com >

--- ros2_ws/src/roscon2023_demo/launch/ROSCon2023Demo.launch.py
+++ ros2_ws/src/roscon2023_demo/launch/ROSCon2023Demo.launch.py
@@ -21,6 +21,7 @@ def launch_setup(context, *args, **kwargs):
     deliberation_dir = os.path.join(get_package_share_directory("otto_deliberation"), "launch")
 
     config_file_arg = LaunchConfiguration("ROS2Con2023Config")
+    use_rviz = LaunchConfiguration("use_rviz")
     config_file = config_file_arg.perform(context)
 
     robots = []
@@ -36,7 +37,8 @@ def launch_setup(context, *args, **kwargs):
                     "lane": robot["lane"],
                     "x_pose": robot["position"]["x"],
                     "y_pose": robot["position"]["y"],
-                    "z_pose": robot["position"]["z"]
+                    "z_pose": robot["position"]["z"],
+                    "tasks_config_file": robot["tasks_config_file"],
                 }
             )
         
@@ -74,14 +76,15 @@ def launch_setup(context, *args, **kwargs):
     ])
 
     deliberation_group = TimerAction(
-        period = 10.,
+        period = 15.,
         actions = [
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
-                    os.path.join(deliberation_dir, 'otto600_deliberation.launch.py')),
+                    os.path.join(deliberation_dir, 'otto_deliberation.launch.py')),
                 launch_arguments = {
                     "namespace" : robot["namespace"],
                     "assigned_lane" : robot["lane"],
+                    "tasks_config_file" : os.path.join(get_package_share_directory("otto_deliberation"), robot["tasks_config_file"]),
                 }.items()
             ) for robot in robots
         ]
@@ -101,7 +104,7 @@ def launch_setup(context, *args, **kwargs):
             os.path.join(o3de_fleet_nav_dir, "o3de_fleet_nav_launch.py")
         ),
         launch_arguments = {
-            "use_rviz": "False",
+            "use_rviz": use_rviz,
             "fleet_config_path": config_file_arg,
         }.items()
     )
@@ -126,6 +129,14 @@ def generate_launch_description():
             'ROS2Con2023Config',
             default_value=os.path.join(ROSCon2023Params, 'config', 'ROSCon2023Config.yaml'),
             description='Full path to the ROS2 parameters file to use for all robot launched nodes'
+        )
+    )
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_rviz",
+            default_value="False",
+            description="Launch rviz with nav2"
         )
     )
 

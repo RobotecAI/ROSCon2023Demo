538ef0eec8758bab6d881cedb254f1604d888cf0
Piotr Jaroszek <piotr.jaroszek@robotec.ai>
2023-10-16 17:04:40 +0200

    Start/stop fleet
    Signed-off-by: Piotr Jaroszek <piotr.jaroszek@robotec.ai>

--- ros2_ws/src/roscon2023_demo/bash/navigate_deliberation.sh
+++ ros2_ws/src/roscon2023_demo/bash/navigate_deliberation.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+
 export ROBOT_NAMESPACE=$1
 export LINE=$2
 export TASK=$3
@@ -8,6 +11,5 @@ echo Staring robot $ROBOT_NAMESPACE
 
 ros2 service call /lock_service lock_service_msgs/srv/Lock "{ key: '${LINE}_GoToPickup', lock_status: false }"
 
-screen -S ${ROBOT_NAMESPACE}_nav2 -dm ros2 launch otto_fleet_nav otto_fleet_nav_launch.py spawn_only_one:="${ROBOT_NAMESPACE}" fleet_config_path:=/home/michalpelka/github/ROSCon2023Demo/ros2_ws/src/roscon2023_demo/config/ROSCon2023Config_900.yaml use_rviz:=True
-
-screen -S  ${ROBOT_NAMESPACE}_deliberation -dm ros2 launch otto_deliberation otto_deliberation.launch.py namespace:="${ROBOT_NAMESPACE}" assigned_lane:="${LINE}" tasks_config_file:=$TASK
+screen -S ${ROBOT_NAMESPACE}_nav2 -dm ros2 launch otto_fleet_nav otto_fleet_nav_launch.py spawn_only_one:="${ROBOT_NAMESPACE}" fleet_config_path:=${SCRIPTPATH}/../config/ROSCon2023Config_900.yaml use_rviz:=False
+screen -S ${ROBOT_NAMESPACE}_deliberation -dm ros2 launch otto_deliberation otto_deliberation.launch.py namespace:="${ROBOT_NAMESPACE}" assigned_lane:="${LINE}" tasks_config_file:=$TASK

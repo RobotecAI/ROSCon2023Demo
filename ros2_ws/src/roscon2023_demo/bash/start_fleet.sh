64b7329c638c76a19ea7a8d0333bb796134dec8d
Piotr Jaroszek <piotr.jaroszek@robotec.ai>
2023-10-16 17:04:40 +0200

    Start/stop fleet
    Signed-off-by: Piotr Jaroszek <piotr.jaroszek@robotec.ai>

--- ros2_ws/src/roscon2023_demo/bash/start_fleet.sh
+++ ros2_ws/src/roscon2023_demo/bash/start_fleet.sh
@@ -1,6 +1,31 @@
-#! /bin/sh
+#! /bin/bash
 
-ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_1', initial_pose: {position: {x: -32.0 , y: 2, z: 0.1}}}"
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
 
-export TaskDir=/home/michalpelka/github/ROSCon2023Demo/ros2_ws/src/otto_deliberation/config/
-./navigate_deliberation.sh otto_1 Line1 ${TaskDir}/otto600_tasks_12.yaml
+# ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_1', initial_pose: {position: {x: -32.0 , y: 2, z: 0.1}}}"
+
+export TaskDir=${SCRIPTPATH}/../../otto_deliberation/config/
+
+
+${SCRIPTPATH}/navigate_deliberation.sh otto_1 Line1 ${TaskDir}/otto600_tasks_12.yaml
+sleep 3
+
+${SCRIPTPATH}/navigate_deliberation.sh otto_2 Line2 ${TaskDir}/otto600_tasks_12.yaml
+sleep 3
+
+${SCRIPTPATH}/navigate_deliberation.sh otto_3 Line3 ${TaskDir}/otto600_tasks_34.yaml
+sleep 3
+
+${SCRIPTPATH}/navigate_deliberation.sh otto_4 Line4 ${TaskDir}/otto600_tasks_34.yaml
+sleep 3
+
+
+while true; do
+    read -n 1 -p "Press 'q' to exit: " input
+    if [ "$input" == "q" ]; then
+        ${SCRIPTPATH}/stop_fleet.sh
+        echo ""
+        break
+    fi
+done

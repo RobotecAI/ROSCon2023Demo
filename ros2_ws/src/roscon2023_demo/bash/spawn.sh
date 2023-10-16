7a07fe8e4ec1768d32a2f21c91f3e4c159c2f71f
Michał Pełka <michal.pelka@robotec.ai>
2023-10-16 17:52:21 +0200

    Adjusments to start all ottos
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>

--- ros2_ws/src/roscon2023_demo/bash/spawn.sh
+++ ros2_ws/src/roscon2023_demo/bash/spawn.sh
@@ -1,5 +1,17 @@
 #! /bin/sh
 
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+
+
+export ConfigDir=${SCRIPTPATH}/../../roscon2023_demo/config/
+
+
+echo Starting MoveIt stacks and deliberation locks
+
+screen -S MOVEIT_LOCKS -dm ros2 launch roscon2023_demo ROSCon2023Demo.launch.py ROS2Con2023Config:=${ConfigDir}/ROSCon2023Config_900.yaml > /tmp/moveit.log
+
+
 ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_1', initial_pose: {position: {x: -32.0 , y: 2, z: 0.1}}}"
 ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_2', initial_pose: {position: {x: -30.0 , y: 2, z: 0.1}}}"
 ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_3', initial_pose: {position: {x: -28.0 , y: 2, z: 0.1}}}"
@@ -11,15 +23,15 @@ ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', r
 ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_8', initial_pose: {position: {x: -26.0 , y: 27, z: 0.1}}}"
 
 
-ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_11', initial_pose: {position: {x: -32.0 , y: 32, z: 0.1}}}"
-ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_12', initial_pose: {position: {x: -30.0 , y: 32, z: 0.1}}}"
-ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_13', initial_pose: {position: {x: -28.0 , y: 32, z: 0.1}}}"
-ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_14', initial_pose: {position: {x: -26.0 , y: 32, z: 0.1}}}"
+ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_21', initial_pose: {position: {x: -32.0 , y: 32, z: 0.1}}}"
+ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_22', initial_pose: {position: {x: -30.0 , y: 32, z: 0.1}}}"
+ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_23', initial_pose: {position: {x: -28.0 , y: 32, z: 0.1}}}"
+ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_24', initial_pose: {position: {x: -26.0 , y: 32, z: 0.1}}}"
 
-ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_15', initial_pose: {position: {x: -32.0 , y: 57, z: 0.1}}}"
-ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_16', initial_pose: {position: {x: -30.0 , y: 57, z: 0.1}}}"
-ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_17', initial_pose: {position: {x: -28.0 , y: 57, z: 0.1}}}"
-ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_18', initial_pose: {position: {x: -26.0 , y: 57, z: 0.1}}}"
+ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_25', initial_pose: {position: {x: -32.0 , y: 57, z: 0.1}}}"
+ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_26', initial_pose: {position: {x: -30.0 , y: 57, z: 0.1}}}"
+ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_27', initial_pose: {position: {x: -28.0 , y: 57, z: 0.1}}}"
+ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_28', initial_pose: {position: {x: -26.0 , y: 57, z: 0.1}}}"
 
 ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_31', initial_pose: {position: {x: -32.0 , y: 62, z: 0.1}}}"
 ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_32', initial_pose: {position: {x: -30.0 , y: 62, z: 0.1}}}"
@@ -30,4 +42,5 @@ ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', r
 ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_36', initial_pose: {position: {x: -30.0 , y: 87, z: 0.1}}}"
 ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_37', initial_pose: {position: {x: -28.0 , y: 87, z: 0.1}}}"
 ros2 service call /spawn_entity gazebo_msgs/srv/SpawnEntity "{name: 'otto600', robot_namespace: 'otto_38', initial_pose: {position: {x: -26.0 , y: 87, z: 0.1}}}"
+
 

cd5a21fda28d586faef5989b0cefb793f6563131
Paweł Liberadzki <pawel.liberadzki@robotec.ai>
2023-10-19 15:40:46 +0200

    Add script for configuring submodules for ros2 workspace.
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>

---
+++ ros2_ws/setup_submodules.bash
@@ -0,0 +1,10 @@
+git submodule init
+git submodule update
+
+if [ "$ROS_DISTRO" == "iron" ]
+then
+	cd ./src/Universal_Robots_ROS2_Driver/
+	git checkout 129687763140508b4b5cb497b197ce1057defe88
+fi
+
+echo "Submodules updated for ROS2 $ROS_DISTRO."

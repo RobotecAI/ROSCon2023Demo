01f5dabf7dd9950cba44a6cf152178d0edb5ffca
Paweł Liberadzki <pawel.liberadzki@robotec.ai>
2023-10-20 08:46:41 +0200

    Update setup submodules script to handle invalid or not supported distribution cases.
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>

--- ros2_ws/setup_submodules.bash
+++ ros2_ws/setup_submodules.bash
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-git submodule init
-git submodule update
-
 if [ "$ROS_DISTRO" == "iron" ]
 then
+	git submodule init
+	git submodule update
 	cd ./src/Universal_Robots_ROS2_Driver/
 	git checkout 129687763140508b4b5cb497b197ce1057defe88
+elif [ "$ROS_DISTRO" == "humble" ]
+then
+	# submodule init and update are sufficient for humble
+	git submodule init
+	git submodule update
+elif [ -z "$ROS_DISTRO" ]
+then
+	echo "ROS 2 distribution not found."
+	exit 1
+else
+	echo "ROS 2 $ROS_DISTRO is not supported in this project - switch to humble or iron distribution."
+	exit 1
 fi
 
 echo "Submodules updated for ROS2 $ROS_DISTRO."
+exit 0

21b6111f68656fa1355cc643e19c0313d48a55c8
Paweł Liberadzki <pawel.liberadzki@robotec.ai>
2023-11-21 11:43:30 +0100

    Change goal type dedicated methods to template method in nav2 action client.
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>

--- ros2_ws/src/otto_deliberation/include/otto_deliberation/nav2_action_client.h
+++ ros2_ws/src/otto_deliberation/include/otto_deliberation/nav2_action_client.h
@@ -17,11 +17,45 @@ public:
     void SendGoal(const NavPath& targetPath, ResultCallback resultCallback, bool goBlind = false, bool reverse = false, bool blindHighSpeed = false);
 
 private:
+    template <typename ActionType>
+    void SendSpecializedGoal(const typename ActionType::Goal& goal, ResultCallback callback, typename rclcpp_action::Client<ActionType>::SharedPtr m_client)
+    {
+        using GoalHandleType = rclcpp_action::ClientGoalHandle<ActionType>;
+        auto sendGoalOptions = typename rclcpp_action::Client<ActionType>::SendGoalOptions();
+        sendGoalOptions.goal_response_callback = [&logger = m_actionLogger, callback](std::shared_ptr<GoalHandleType> future)
+        {
+            auto goalHandle = future.get();
+            if (!goalHandle)
+            {
+                RCLCPP_ERROR(logger, "Goal was rejected by server");
+                callback(false);
+            }
+            else
+            {
+                RCLCPP_INFO(logger, "Goal accepted by server, waiting for result");
+            }
+        };
+        sendGoalOptions.feedback_callback = [](typename GoalHandleType::SharedPtr, const std::shared_ptr<const typename GoalHandleType::Feedback>)
+        {
+        };
+        sendGoalOptions.result_callback = [&logger = m_actionLogger, callback](const typename GoalHandleType::WrappedResult& result)
+        {
+            switch (result.code)
+            {
+            case rclcpp_action::ResultCode::SUCCEEDED:
+                callback(true);
+                break;
+            default:
+                RCLCPP_ERROR(logger, "Goal did not succeed");
+                callback(false);
+                return;
+            };
+        };
+        m_client->async_send_goal(goal, sendGoalOptions);
+    }
+
     rclcpp_action::Client<Nav2Action>::SharedPtr m_nav2Client;
     rclcpp_action::Client<FollowPathAction>::SharedPtr m_followClient;
-
-    void SendNav2Goal(const Nav2Action::Goal& goal, ResultCallback callback);
-    void SendBlindGoal(const FollowPathAction::Goal& goal, ResultCallback callback);
 
     rclcpp::Logger m_actionLogger;
 };

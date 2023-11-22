c698ba48ba8507b90706464481b65c71553d2600
Paweł Liberadzki <pawel.liberadzki@robotec.ai>
2023-11-22 11:38:23 +0100

    Make cleanup in ur palletization task constructor.
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>

--- ros2_ws/src/ur_palletization/src/taskConstructor.h
+++ ros2_ws/src/ur_palletization/src/taskConstructor.h
@@ -1,55 +1,52 @@
-#include <rclcpp/node.hpp>
-#include <rclcpp/rclcpp.hpp>
-#include <vector>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
-#include <tf2_eigen/tf2_eigen.hpp>
-
-
-#include "vision.h"
-#include "utils.h"
-#include <moveit/robot_model_loader/robot_model_loader.h>
-#include <moveit/move_group_interface/move_group_interface.h>
-#include "constants.h"
-
 #pragma once
 
-namespace Palletization {
+#include "constants.h"
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <rclcpp/node.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <tf2_eigen/tf2_eigen.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include "utils.h"
+#include "vision.h"
 
+#include <memory>
+#include <vector>
+
+namespace Palletization
+{
     static const rclcpp::Logger LOGGER = rclcpp::get_logger("mtc");
 
-    class RoboticArmController {
+    class RoboticArmController
+    {
     public:
         static constexpr char PickupPoseName[] = "pickup";
         static constexpr char LiftPoseName[] = "lift";
         static constexpr char DropPoseName[] = "drop";
 
-        RoboticArmController(std::shared_ptr<moveit::planning_interface::MoveGroupInterface> moveGroupInterface,
-                             std::string ns);
+        RoboticArmController(std::shared_ptr<moveit::planning_interface::MoveGroupInterface> moveGroupInterface, const std::string& ns);
 
-        rclcpp::node_interfaces::NodeBaseInterface::SharedPtr getNodeBaseInterface();
+        bool SetPosePIP(const std::string &poseName);
 
-        bool setPosePIP(const std::string &poseName);
+        bool SetPosePIP(
+            const Eigen::Vector3d &tcpPosition,
+            const Eigen::Quaterniond &tcpOrientation,
+            float speed = 0.75f,
+            const std::string& interpolation = "PTP");
 
-        bool
-        setPosePIP(const Eigen::Vector3d &tcp_position, const Eigen::Quaterniond &tcp_orientation, float speed = 0.75f,
-                   const std::string &interpolation = "PTP");
-
-        Eigen::Quaterniond getCurrentOrientation();
-
+        Eigen::Quaterniond GetCurrentOrientation() const;
 
     private:
         using PredefinePoseJointSpace = std::map<std::string, double>;
-        std::unordered_map<std::string, PredefinePoseJointSpace> m_predefinedPoses;
-
-        constexpr static float change = 0.35f;
-        std::map<std::string, double> DropConfig;
 
         // Compose an MTC task from a series of stages.
-        rclcpp::Node::SharedPtr node_;
+        rclcpp::Node::SharedPtr node;
+        std::map<std::string, double> dropConfig;
 
+        std::unordered_map<std::string, PredefinePoseJointSpace> m_predefinedPoses;
         std::vector<std::string> namesOfBoxes;
 
-        std::shared_ptr<moveit::planning_interface::MoveGroupInterface> m_move_groupIterface;
+        std::shared_ptr<moveit::planning_interface::MoveGroupInterface> m_moveGroupInterface;
         std::string ns;
     };
 } // namespace TaskConstructor

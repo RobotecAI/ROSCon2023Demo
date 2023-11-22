3e87bea394bf49a12c90a6a34ddb57530b58e986
Paweł Liberadzki <pawel.liberadzki@robotec.ai>
2023-11-22 11:12:37 +0100

    Make minor code style adjustment in ur palletization gripper.
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>

--- ros2_ws/src/ur_palletization/src/gripper.cpp
+++ ros2_ws/src/ur_palletization/src/gripper.cpp
@@ -1,10 +1,12 @@
 #include "gripper.h"
 
+#include <rclcpp_action/create_client.hpp>
+
 namespace Gripper
 {
     GripperController::GripperController(std::shared_ptr<rclcpp::Node> node, const std::string& topic)
     {
-        m_client_ptr = rclcpp_action::create_client<control_msgs::action::GripperCommand>(node, topic);
+        m_client = rclcpp_action::create_client<control_msgs::action::GripperCommand>(node, topic);
     }
 
     bool GripperController::SendGripCommand(bool shouldAttach)
@@ -13,10 +15,10 @@ namespace Gripper
         goal.command.position = shouldAttach ? 0.0 : 1.0;
         goal.command.max_effort = 10000.0;
 
-        auto future = m_client_ptr->async_send_goal(goal);
+        auto future = m_client->async_send_goal(goal);
         auto goalHandle = future.get();
 
-        auto resultFuture = m_client_ptr->async_get_result(goalHandle);
+        auto resultFuture = m_client->async_get_result(goalHandle);
         auto result = resultFuture.get().result;
 
         return result->reached_goal;

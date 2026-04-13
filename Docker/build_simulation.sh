a9a17396a086c7ff0945abe46cac1b4224211a5f
Jan Hanca <jan.hanca@robotec.ai>
2026-04-13 13:23:05 +0200

    Separate clone step
    Signed-off-by: Jan Hanca <jan.hanca@robotec.ai>

--- Docker/build_simulation.sh
+++ Docker/build_simulation.sh
@@ -6,12 +6,12 @@
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 #
 
-# Validate the ROSCON_DEMO_LEVEL value before starting the longer git processes
+# Derive the DEMO_LEVEL from ROSCON_DEMO_LEVEL (validation was done in clone_simulation.sh)
 if [ "$ROSCON_DEMO_LEVEL" == 'level1' ]
 then
     DEMO_LEVEL=demolevel1
 elif [ "$ROSCON_DEMO_LEVEL" == 'level2' ]
-then    
+then
     DEMO_LEVEL=demolevel2
 else
     echo "Invalid 'level' argument '$ROSCON_DEMO_LEVEL'. Must be level1 or level2."
@@ -20,52 +20,6 @@ fi
 
 # Initialize ROS
 . /opt/ros/${ROS_DISTRO}/setup.sh
-
-
-###############################################################
-# Clone and register the ROSCon2023Demo
-###############################################################
-git config --global http.postBuffer 524288000
-git config --global http.lowSpeedLimit 0
-git config --global http.lowSpeedTime 300
-
-echo "Cloning the RosCon2023Demo project"
-git clone --single-branch -b $ROSCON_DEMO_BRANCH $ROSCON_DEMO_REPO $ROSCON_DEMO_ROOT && \
-    git -C $ROSCON_DEMO_ROOT lfs install && \
-    git -C $ROSCON_DEMO_ROOT lfs pull && \
-    git -C $ROSCON_DEMO_ROOT reset --hard $ROSCON_DEMO_COMMIT
-if [ $? -ne 0 ]
-then
-    echo "Error cloning RosCon2023Demo project $ROSCON_DEMO_REPO"
-    exit 1
-fi
-
-echo "Initializing submodules"
-git -C $ROSCON_DEMO_ROOT submodule update --init --recursive
-if [ $? -ne 0 ]
-then
-    echo "Error initializing submodules"
-    exit 1
-fi
-
-$ROSCON_DEMO_ROOT/engine/o3de/python/get_python.sh
-if [ $? -ne 0 ]
-then
-    echo "Error downloading/configuring O3DE Python"
-    exit 1
-fi
-
-$ROSCON_DEMO_ROOT/engine/o3de/scripts/o3de.sh register -ep $ROSCON_DEMO_ROOT/engine/o3de
-if [ $? -ne 0 ]
-then
-    echo "Error registering the O3DE engine"
-    exit 1
-fi
-
-###############################################################
-# Set the filename of the level in the registry file
-###############################################################
-sed -i "s/demolevel1/${DEMO_LEVEL}/g" $ROSCON_DEMO_PROJECT/Registry/load_level.setreg
 
 ###############################################################
 # Build and install the additional ROS 2 packages and drivers
